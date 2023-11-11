d6f33e5d4b3c15c8d9b8d3a4750d2d8422517ae8
Barribob Development <barribobdevelopment@gmail.com>
2023-11-11 16:44:33 -0700

    attack animation

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,41 +5,62 @@ class_name Player
 signal on_end_invulnerability
 signal update_health(health, max_health)
 
-const STOP_LAG = 8.0
+const STOP_LAG = 16.0
 const MOVE_LAG = 16.0
 const WALK_SPEED = 300.0
+const ATTACK_VELOCITY = 1000.0
 
 @onready var damageable: Damageable = $Damageable
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var facing_direction: Node2D = $FacingDirection
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var movement_enabled = true
+var attack_enabled = true
 var last_direction = Vector2.RIGHT
 
 func _process(_delta):
-    if not movement_enabled:
-        return
+    if movement_enabled:
+        handle_sprite_movement()
+    if attack_enabled:
+        handle_attack_input()
 
+func handle_attack_input():
+    var attack_direction = Input.get_vector("attack_left", "attack_right", "attack_up", "attack_down")
+    if attack_direction.length() > 0:
+        facing_direction.look_at(facing_direction.global_position + attack_direction)
+        animation_player.play("attack")
+        velocity += attack_direction * ATTACK_VELOCITY
+
+func disable_attack():
+    attack_enabled = false
+
+func enable_attack():
+    attack_enabled = true
+
+func handle_sprite_movement():
     var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
     if direction.length() > 0:
-        $AnimatedSprite2D.animation = "walk"
-        $AnimatedSprite2D.play()
+        animated_sprite.animation = "walk"
+        animated_sprite.play()
         last_direction = direction
     else:
-        $AnimatedSprite2D.stop()
-        $AnimatedSprite2D.animation = "idle"
+        animated_sprite.stop()
+        animated_sprite.animation = "idle"
 
     if direction.x != 0:
-        $AnimatedSprite2D.flip_v = false
-        $AnimatedSprite2D.flip_h = direction.x < 0
-    # elif direction.y != 0:
-        # $AnimatedSprite2D.flip_v = direction.y > 0
+        animated_sprite.flip_v = false
+        animated_sprite.flip_h = direction.x < 0
 
 func _physics_process(delta):
     var speed = WALK_SPEED
 
-    var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-    if movement_enabled and direction != Vector2.ZERO:
+    var direction = Vector2.ZERO
+    if movement_enabled:
+        direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+    if direction != Vector2.ZERO:
         velocity = lerp(velocity, direction * speed, delta * MOVE_LAG)
     else:   
         velocity = lerp(velocity, Vector2.ZERO, delta * STOP_LAG)
