f16b5ba69dd67020288c615cbfe00bff6dff4088
Barribob Development <barribobdevelopment@gmail.com>
2023-11-19 13:05:41 -0700

    change health bar to use whole hearts

--- HealthBar.gd
+++ HealthBar.gd
@@ -1,7 +1,6 @@
 extends HBoxContainer
 
 @export var heart_full: Texture2D
-@export var heart_half: Texture2D
 @export var heart_empty: Texture2D
 
 @export var hearts_to_show = 5
@@ -13,11 +12,9 @@ func _ready():
 
 func update_health(health, max_health):
     for i in get_child_count():
-        if health > i * 2 + 1:
+        if health > i:
             get_child(i).texture = heart_full
-        elif health > i * 2:
-            get_child(i).texture = heart_half
-        elif max_health > i * 2:
+        elif max_health > i:
             get_child(i).texture = heart_empty
         else:
             get_child(i).texture = null
