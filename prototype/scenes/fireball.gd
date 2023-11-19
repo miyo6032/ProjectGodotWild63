6aa1e9ad19a1d91c5a870981c696d973ea39a046
Barribob Development <barribobdevelopment@gmail.com>
2023-11-19 11:25:29 -0700

    add explosion radius to rockets

--- prototype/scenes/fireball.gd
+++ prototype/scenes/fireball.gd
@@ -2,18 +2,20 @@ extends RigidBody2D
 
 @export var knockback: float
 @export var explosion_scene: PackedScene
+@onready var explosion_area = $ExplosionArea
 
-func _on_area_2d_area_entered(area:Area2D):
-    if area is Damageable and !area.dashing:
-        area.damage({damage = 1, knockback = (area.global_position - global_position) * knockback})
-        spawn_explosion()
-        queue_free()
+func _on_area_2d_area_entered(_area:Area2D):
+    spawn_explosion()
 
 func _on_area_2d_body_entered(_body:Node2D):
     spawn_explosion()
-    queue_free()
 
 func spawn_explosion():
+    for area in explosion_area.get_overlapping_areas():
+        area.damage_ignore_dashing({damage = 1, knockback = (area.global_position - global_position).normalized() * knockback})
+
     var explosion = explosion_scene.instantiate()
     get_parent().add_child(explosion)
     explosion.global_position = global_position
+
+    queue_free()
