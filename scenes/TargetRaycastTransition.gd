6fb518427bfad67519903bd01f7f474631a6a868
Pyrocreep <pyrocreeperking@gmail.com>
2023-11-19 18:30:32 +0100

    more levels

--- scenes/TargetRaycastTransition.gd
+++ scenes/TargetRaycastTransition.gd
@@ -5,7 +5,7 @@ class_name TargetRaycastTransition
 @export var raycast2d : RayCast2D
 
 func get_decision() -> bool:
-    var player = state_data.player
-    var enemy = state_data.enemy
-    raycast2d.target_position = player.global_position - enemy.global_position
-    return raycast2d.is_colliding()
+	var player = state_data.player
+	var enemy = state_data.enemy
+	raycast2d.target_position = player.global_position - enemy.global_position
+	return raycast2d.is_colliding()
