a2adcde9667108bf923e99cd7dc3d04bc7efa58b
Barribob Development <barribobdevelopment@gmail.com>
2023-11-19 09:10:37 -0700

    add raycast test to explosion

--- TrashBag.gd
+++ TrashBag.gd
@@ -7,22 +7,35 @@ extends Node2D
 @onready var damageable = $Damageable
 
 var is_exploded: bool
+var did_raycast: bool
+
+func _ready():
+    Console.add_command("explode", explode)
 
 func _on_damageable_on_damage(_amount):
+    explode()
+
+func explode():
     if is_exploded:
         return
 
     is_exploded = true
-    for area in explosion_area.get_overlapping_areas():
-        if area != damageable:
-            area.damage_ignore_dashing({damage = 1, knockback = (area.global_position - global_position).normalized() * explosion_knockback})
 
     var particles: CPUParticles2D = particles_scene.instantiate()
     particles.global_position = global_position
     get_parent().add_child(particles)
 
-    await get_tree().create_timer(0.2).timeout
-    queue_free()
+    var tween = create_tween()
+    tween.tween_callback(queue_free).set_delay(0.2)
 
-func _on_explosion_area_area_entered(area:Area2D):
-    print(area.name)
+func _physics_process(_delta):
+    if is_exploded and not did_raycast:
+        did_raycast = true
+        for area in explosion_area.get_overlapping_areas():
+            if area != damageable:
+                var space_state = get_world_2d().direct_space_state
+                var query = PhysicsRayQueryParameters2D.create(global_position, area.global_position)
+                query.exclude = [damageable, area]
+                var result = space_state.intersect_ray(query)
+                if !result:
+                    area.damage_ignore_dashing({damage = 1, knockback = (area.global_position - global_position).normalized() * explosion_knockback})
