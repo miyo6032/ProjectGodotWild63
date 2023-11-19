d07b3b92ce24bf945c19144f83543c433d6ce435
Barribob Development <barribobdevelopment@gmail.com>
2023-11-18 17:35:16 -0700

    add particles to trash bag explosion

--- TrashBag.gd
+++ TrashBag.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var explosion_knockback: float
+@export var particles_scene: PackedScene
 
 @onready var explosion_area = $ExplosionArea
 @onready var damageable = $Damageable
@@ -12,6 +13,10 @@ func _on_damageable_on_damage(_amount):
     for area in explosion_area.get_overlapping_areas():
         if area != damageable:
             area.damage_ignore_dashing({damage = 1, knockback = (area.global_position - global_position).normalized() * explosion_knockback})
+
+    var particles: CPUParticles2D = particles_scene.instantiate()
+    particles.global_position = global_position
+    get_parent().add_child(particles)
 
     await get_tree().create_timer(0.2).timeout
     queue_free()
